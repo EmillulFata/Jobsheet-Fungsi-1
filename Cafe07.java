efa4068a48f0b4bb6d99c82f0d4504d4e4058e60
FataRach <emillulfr@gmail.com>
2025-12-01 19:01:47 +0700

    Modifikasi Percobaan 1

--- Cafe07.java
+++ Cafe07.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class Cafe07 {
-
-  public static void Menu() {
+  public static void main(String[] args) {
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam  - Rp 15,000");
         System.out.println("2. Cappuccino  - Rp 20,000");
@@ -12,8 +11,6 @@ public class Cafe07 {
         System.out.println("6. Mie Goreng  - Rp 18,000");
         System.out.println("============================");
         System.out.println("Silakan pilih menu yang anda inginkan!");
-    }
-  public static void main(String[] args) {
-    Menu();
-    }
+     }
   }
+
